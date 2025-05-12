4a21f7f4650f33fae9c41758f584a9a84f7478bc
Niko Rahmadi Wiharto <nikorahmadiwiharto@gmail.com>
2025-05-12 20:47:20 +0700

    update file link di index html

--- jumlahGanjil.js
+++ jumlahGanjil.js
@@ -1,13 +1,24 @@
-let numbers = [1, 2, 3, 4, 5, 5];
+let numbers = [10, 1];
 
 
 // Dengan filter dan reduce
 // Filter untuk menampung angka-angka ganjil
 // Reduce untuk menjumlahkan setiap value pada array yang dihasilkan filter
-let jumlahGanjil = (numbers) => 
-    numbers
-    .filter(n => n % 2 === 1)
-    .reduce((total, n) => total + n, 0);
+// let jumlahGanjil = (numbers) => 
+//     numbers
+//     .filter(angka => angka % 2 === 1)
+//     .reduce((total, angka) => total + angka);
 
-console.log(jumlahGanjil(numbers));
+// console.log(jumlahGanjil(numbers));
 
+// Menggunakan forEach
+
+let jumlahGanjil = (numbers) => {
+    let nilai = 0;
+    numbers.forEach((angka) => {
+        if (angka % 2 == 1){
+            nilai += angka;
+        }
+    });
+    return nilai;
+};
