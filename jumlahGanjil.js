56d0486fd400ca6f0f4f6bde0a3b8dfc04f1a738
Niko Rahmadi Wiharto <nikorahmadiwiharto@gmail.com>
2025-05-12 21:06:27 +0700

    finalisasi file jumlahGanjil.js

--- jumlahGanjil.js
+++ jumlahGanjil.js
@@ -1,24 +1,26 @@
-let numbers = [10, 1];
+let numbers = [10, 11];
 
 
 // Dengan filter dan reduce
 // Filter untuk menampung angka-angka ganjil
 // Reduce untuk menjumlahkan setiap value pada array yang dihasilkan filter
-// let jumlahGanjil = (numbers) => 
-//     numbers
-//     .filter(angka => angka % 2 === 1)
-//     .reduce((total, angka) => total + angka);
+let jumlahGanjil = (numbers) => {
+    let totals = 0;
+    totals = numbers
+    .filter(angka => angka % 2 === 1)
+    .reduce((total, angka) => total + angka, 0); 
+    // 0 adalah intial value (jika tidak terdapat nilai ganjil di array)
 
-// console.log(jumlahGanjil(numbers));
-
+    console.log((totals > 0)? `Jumlah angka dari semua ganjil adalah ${totals}`: 'tidak ada angka ganjil');
+}
 // Menggunakan forEach
 
-let jumlahGanjil = (numbers) => {
+let jumlahGanjil1 = (numbers) => {
     let nilai = 0;
     numbers.forEach((angka) => {
         if (angka % 2 == 1){
             nilai += angka;
         }
     });
-    return nilai;
+    console.log((nilai > 0)? `Jumlah angka dari semua ganjil adalah ${nilai}`: 'tidak ada angka ganjil');
 };
